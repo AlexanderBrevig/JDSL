63256c50f8323448cebf3d950bbb204ea9ec00e0
Alexander Brevig <alexanderbrevig@gmail.com>
2023-06-07 02:50:14 +0200

    lexer.this.readInt

--- lexer.js
+++ lexer.js
@@ -1,6 +1,6 @@
-export const Lexer_readIdent = () => {
+export const Lexer_readInt = () => {
   const pos = Lexer_this_position.value;
-  while (Lexer_isAlpha()) {
+  while (Lexer_isNumeric()) {
     Lexer_readChar();
   }
   return Lexer_this_input.value.slice(pos, Lexer_this_position.value);
