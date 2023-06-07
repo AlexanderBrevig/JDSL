6a3cedfd691b5644c120faff5a5a7e4fa3b50559
Alexander Brevig <alexanderbrevig@gmail.com>
2023-06-07 02:48:29 +0200

    lexer.this.constructor

--- lexer.js
+++ lexer.js
@@ -1,1 +1,4 @@
-export let Lexer_this_position = { value: 0 };
+export const Lexer_constructor = () => {
+  Lexer_this_position.value = 0;
+  Lexer_this_ch.value = Lexer_this_input.value[Lexer_this_position.value];
+};
