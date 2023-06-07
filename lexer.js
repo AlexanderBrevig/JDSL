c0b93b550ebb5744448c820ddc1f9756405bbf6f
Alexander Brevig <alexanderbrevig@gmail.com>
2023-06-07 02:49:08 +0200

    lexer.this.nextToken

--- lexer.js
+++ lexer.js
@@ -1,4 +1,25 @@
-export const Lexer_constructor = () => {
-  Lexer_this_position.value = 0;
-  Lexer_this_ch.value = Lexer_this_input.value[Lexer_this_position.value];
+export const Lexer_nextToken = () => {
+  Lexer_skipWhitespace();
+
+  let token = Lexer_tokenSet.has(Lexer_this_ch.value) ? (Lexer_this_ch.value) : null;
+
+  if (token === null) {
+    if (Lexer_isAlpha()) {
+      Lexer_lookupIdent_ident = Lexer_readIdent();
+      token = Lexer_lookupIdent();
+      return token;
+    }
+
+    if (Lexer_isNumeric()) {
+      token = Lexer_tokens.INT;
+      const intVal = Lexer_readInt();
+      return { type: token, value: intVal };
+    }
+
+    token = Lexer_this_ch.value === "" ? Lexer_tokens.EOF : Lexer_tokens.ILLEGAL;
+  }
+
+  Lexer_readChar();
+
+  return token;
 };
