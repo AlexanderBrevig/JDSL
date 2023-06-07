241d82c737e07b18bc1e2e9ea8547ad49b73f284
Alexander Brevig <alexanderbrevig@gmail.com>
2023-06-07 02:51:36 +0200

    lexer.this.lookupIdent

--- lexer.js
+++ lexer.js
@@ -1,1 +1,3 @@
-export var Lexer_lookupIdent_ident = "";
+export const Lexer_lookupIdent = () => {
+  return Lexer_keywords[Lexer_lookupIdent_ident] || { type: Lexer_tokens.IDENT, value: Lexer_lookupIdent_ident };
+};
