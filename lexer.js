0b7467d6ddfec9288e4ee1af9c0fb05a676f4243
Alexander Brevig <alexanderbrevig@gmail.com>
2023-06-07 02:50:25 +0200

    lexer.this.isAlpha

--- lexer.js
+++ lexer.js
@@ -1,7 +1,3 @@
-export const Lexer_readInt = () => {
-  const pos = Lexer_this_position.value;
-  while (Lexer_isNumeric()) {
-    Lexer_readChar();
-  }
-  return Lexer_this_input.value.slice(pos, Lexer_this_position.value);
+export const Lexer_isAlpha = () => {
+  return Lexer_this_ch.value >= "a" && Lexer_this_ch.value <= "z" || Lexer_this_ch.value >= "A" && Lexer_this_ch.value <= "Z" || Lexer_this_ch.value === "_";
 };
